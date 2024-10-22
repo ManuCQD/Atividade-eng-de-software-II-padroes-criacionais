b1f19900b82d91ab18148c5f9027bdaafa7b16e9
Manuelli Duarte <manuelliduartecontato@gmail.com>
2024-10-22 16:40:34 -0300

    primeiro commit

---
+++ Arquivos.py
@@ -0,0 +1,12 @@
+# arquivos.py
+class TxtArquivo:
+    def ler_conteudo(self):
+        print("Arquivo TXT!!!")
+
+class DocxArquivo:
+    def ler_conteudo(self):
+        print("Arquivo DOCX!!!")
+
+class PdfArquivo:
+    def ler_conteudo(self):
+        print("Arquivo PDF!!!")
---
+++ Factory_Met.py
@@ -0,0 +1,17 @@
+from Arquivos import TxtArquivo, DocxArquivo, PdfArquivo  # Importa as classes corretamente
+
+# Verifica o tipo de arquivo e retorna a classe correspondente
+
+def criar_arquivos(tipo_arquivo):  # Verifica o tipo de arquivo e retorna a classe correspondente
+    if tipo_arquivo == "txt":
+        return TxtArquivo()  #caso verdeiro ele ira retorna uma instância da classe TxtArquivo
+    elif tipo_arquivo == "docx":
+        return DocxArquivo()  #caso verdeiro ele ira retorna uma instância da classe docx
+    elif tipo_arquivo == "pdf":
+        return PdfArquivo()  #caso verdeiro ele ira #caso verdeiro ele ira retorna uma instância da classe pdf
+    else:
+        print("Tipo de arquivo não encontrado")
+        
+
+arquivo = criar_arquivos('pdf')  # passamos como entrada "pdf" então com 4 e 5 linha são verdadeiras, sendo assim ele retornara uma instancia da classe pdfarquivo
+arquivo.ler_conteudo()  # Chama o método para ler o conteúdo
